8900b5f85fd6a4d1c893820fd05f3ca4224f632b
jhb1993 <jberryhill199@gmail.com>
2018-04-05 18:28:49 -0600

    Made gen alg more robust- JHB

--- AoEGenAlg.m
+++ AoEGenAlg.m
@@ -6,8 +6,8 @@ clc
 clear
 
 chromosome_size = 49; % If chromosome length changes must change mutation function with it
-generation_size = 10;
-M = 1000; % Total Number of generations
+generation_size = 10; % MUST BE AN EVEN NUMBER!!!!!!!!!
+M = 20; % Total Number of generations
 current_gen = 1; %Will need to keep track of the generaation we are on for mutation to work properly
 
 
@@ -26,93 +26,110 @@ for counter_1=1:size(generation_chromos,1)
     fitness(counter_1,2) = counter_1;
 end
 
+%plot initial
+figure(1), clf,
+plot(current_gen-1,fitness(:,1),'k*')
+hold on
+axis([0 M 0 1600])
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% LET THE HUNGER GAMES BEGIN!!!!
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 %perform tournament selection for mother and father chromosomes
-tournament_size = 3;
+tournament_size = 5;
 for master_counter=1:M
-for counter_3=1:2:generation_size-1
-    for counter_2=1:2
-        %randomly select chromosomes
-        rand_chromo_nums = randperm(generation_size,tournament_size);
+    
+    for counter_3=1:2:generation_size-1 %GEN_SIZE MUST BE EVEN!!!!
+        
+        for counter_2=1:2
+            
+            %randomly select chromosomes
+            rand_chromo_nums = randperm(generation_size,tournament_size);
 
-        tournament_chromos = zeros(tournament_size,chromosome_size);
-        tournament_fitnesses = zeros(tournament_size,1);
-        for counter_1=1:tournament_size
-            tournament_chromos(counter_1,:) = generation_chromos(rand_chromo_nums(counter_1),:);
-            tournament_fitnesses(counter_1) = fitness(rand_chromo_nums(counter_1));
-        end
+            tournament_chromos = zeros(tournament_size,chromosome_size);
+            tournament_fitnesses = zeros(tournament_size,1);
+            
+            for counter_1=1:tournament_size
+                tournament_chromos(counter_1,:) = generation_chromos(rand_chromo_nums(counter_1),:);
+                tournament_fitnesses(counter_1) = fitness(rand_chromo_nums(counter_1));
+            end
 
-        %determine champion chromosome
-        highest_tournament_fitness = max(tournament_fitnesses);
-        highest_fitness_chromos = [];
+            %determine champion chromosome
+            highest_tournament_fitness = max(tournament_fitnesses);
+            highest_fitness_chromos = [];
 
-        for counter_1=1:tournament_size
-            if tournament_fitnesses(counter_1)==highest_tournament_fitness
-                highest_fitness_chromos=[highest_fitness_chromos,rand_chromo_nums(counter_1)];
+            for counter_1=1:tournament_size
+                if tournament_fitnesses(counter_1)==highest_tournament_fitness
+                    highest_fitness_chromos=[highest_fitness_chromos,rand_chromo_nums(counter_1)];
+                end
+            end
+
+            if length(highest_fitness_chromos)==1
+                if counter_2==1
+                    mother_chromo = generation_chromos(highest_fitness_chromos(1),:);
+                elseif counter_2==2
+                    father_chromo = generation_chromos(highest_fitness_chromos(1),:);
+                end
+            elseif length(highest_fitness_chromos)>1
+                if counter_2==1
+                    mother_chromo = generation_chromos(highest_fitness_chromos(randperm(length(highest_fitness_chromos),1)),:);
+                elseif counter_2==2
+                    father_chromo = generation_chromos(highest_fitness_chromos(randperm(length(highest_fitness_chromos),1)),:);
+                end
             end
         end
 
-        if length(highest_fitness_chromos)==1
-            if counter_2==1
-                mother_chromo = generation_chromos(highest_fitness_chromos(1),:);
-            elseif counter_2==2
-                father_chromo = generation_chromos(highest_fitness_chromos(1),:);
-            end
-        elseif length(highest_fitness_chromos)>1
-            if counter_2==1
-                mother_chromo = generation_chromos(highest_fitness_chromos(randperm(length(highest_fitness_chromos),1)),:);
-            elseif counter_2==2
-                father_chromo = generation_chromos(highest_fitness_chromos(randperm(length(highest_fitness_chromos),1)),:);
-            end
+
+        % Crossover
+        cross_prob = .6; % We can change
+        cross_rand = rand(1);
+        if cross_rand < cross_prob
+            cross_point = ceil(chromosome_size*rand(1));
+            child_1 = [mother_chromo(1:cross_point),father_chromo(cross_point+1:end)];
+            child_2 = [father_chromo(1:cross_point),mother_chromo(cross_point+1:end)];
+        else
+            child_1 = mother_chromo;
+            child_2 = father_chromo;
         end
+
+        % Mutation
+        mut_prob = .1; % We can change
+        child_1 = mutation(child_1, chromosome_size, M, current_gen, mut_prob);
+        child_2 = mutation(child_2, chromosome_size, M, current_gen, mut_prob);
+
+        %create next generation chromo matrix and fitness matrix
+        next_gen_chromos(counter_3,:)=child_1;
+        next_gen_fitness(counter_3,1)=AoEModel(child_1);
+        next_gen_fitness(counter_3,2)=generation_size+counter_3;
+        next_gen_chromos(counter_3+1,:)=child_2;
+        next_gen_fitness(counter_3+1,1)=AoEModel(child_2);
+        next_gen_fitness(counter_3+1,2)=1+generation_size+counter_3;
     end
 
+    %%%elitism
+    elitism_fitness = [fitness;next_gen_fitness];
+    elitism_fitness = sortrows(elitism_fitness,1);
 
-    % Crossover
-
-    cross_prob = .6; % We can change this, set high to test cross over
-    cross_rand = rand(1);
-    if cross_rand < cross_prob
-        cross_point = ceil(chromosome_size*rand(1));
-        child_1 = [mother_chromo(1:cross_point),father_chromo(cross_point+1:end)];
-        child_2 = [father_chromo(1:cross_point),mother_chromo(cross_point+1:end)];
-    else
-        child_1 = mother_chromo;
-        child_2 = father_chromo;
-    end
-
-    % Mutation
-    mut_prob = .1; % We can change, set high to test
-    % child_1_save = child_1; % Used to compare changes
-    child_1 = mutation(child_1, chromosome_size, M, current_gen, mut_prob);
-    % child_comp = [child_1_save;child_1]; % Used to compare changes
-    child_2 = mutation(child_2, chromosome_size, M, current_gen, mut_prob);
-
-    next_gen_chromos(counter_3,:)=child_1;
-    next_gen_fitness(counter_3,1)=AoEModel(child_1);
-    next_gen_fitness(counter_3,2)=10+counter_3;
-    next_gen_chromos(counter_3+1,:)=child_2;
-    next_gen_fitness(counter_3+1,1)=AoEModel(child_2);
-    next_gen_fitness(counter_3+1,2)=11+counter_3;
+    for counter_4=generation_size+1:2*generation_size
+        if elitism_fitness(counter_4,2)<generation_size+1
+            generation_chromos(counter_4-generation_size,:)=...
+                generation_chromos(elitism_fitness(counter_4,2),:);
+            fitness(counter_4-generation_size,1)=fitness(elitism_fitness(counter_4,2),1);
+        else
+            generation_chromos(counter_4-generation_size,:)=...
+                next_gen_chromos(elitism_fitness(counter_4,2)-generation_size,:);
+            fitness(counter_4-generation_size,1)=...
+                next_gen_fitness(elitism_fitness(counter_4,2)-generation_size,1);
+        end
+    end    
+    
+    current_gen = current_gen+1;
+    plot(current_gen-1,fitness(:,1),'k*')
 end
 
-%%%elitism
-elitism_fitness = [fitness;next_gen_fitness];
-elitism_fitness = sortrows(elitism_fitness,1);
+max(fitness)
 
-for counter_4=11:20
-    if elitism_fitness(counter_4,2)<11
-        generation_chromos(counter_4-10,:)=generation_chromos(elitism_fitness(counter_4,2),:);
-        fitness(counter_4-10,1)=fitness(elitism_fitness(counter_4,2),1);
-    else
-        generation_chromos(counter_4-10,:)=next_gen_chromos(elitism_fitness(counter_4,2)-10,:);
-        fitness(counter_4-10,1)=next_gen_fitness(elitism_fitness(counter_4,2)-10,1);
-    end
-end
-
-current_gen = current_gen+1;
-end
+%I just put this in so you can run the optimal chromosome and see how many
+%of each unit got trained, etc.
+AoEModel(generation_chromos(end,:))
